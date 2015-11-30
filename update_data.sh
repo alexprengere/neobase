d52118df8f89f1723d8b362f02ce922cae6ec15b
Alex <alexprengere@gmail.com>
2015-11-30 03:09:27 -0500

    Move update script outside of pkg

--- update_data.sh
+++ update_data.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
+DIR=`dirname $0`
+cd "$DIR/neobase"
+
 rm -f optd_por_public.csv
 wget 'https://raw.githubusercontent.com/opentraveldata/opentraveldata/master/opentraveldata/optd_por_public.csv'
