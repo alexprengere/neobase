20f4bde1aa64f0a72d77aefe2287d36306fb5002
Alex <alexprengere@gmail.com>
2016-03-07 03:08:48 -0500

    Fix tests

--- neobase/neobase.py
+++ neobase/neobase.py
@@ -425,7 +425,7 @@ class NeoBase(object):
         >>> len(list(b.find_with(c0)))
         17
         >>> len(list(b.find_with(c1)))
-        146
+        147
         >>> len(list(b.find_with(c0 + c1)))
         2
         """
