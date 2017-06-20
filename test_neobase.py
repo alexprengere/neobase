b0f0da34559dd25f0d12d84d90489cfc27b8d661
Alex <alexprengere@gmail.com>
2017-06-20 08:02:55 -0400

    Update tests

--- test_neobase.py
+++ test_neobase.py
@@ -10,6 +10,15 @@ def test_distance(base):
     assert base.distance('ORY', 'CDG') - 34.8747 < 1e-3
 
 
+def test_airport_priority_over_city(base):
+    assert base.get('NCE', 'name') == "Nice Côte d'Azur International Airport"
+    assert base.get('MAD', 'name') == "Adolfo Suárez Madrid–Barajas Airport"
+
+
+def test_obsolete_por_are_not_loaded(base):
+    assert base.get('KMG', 'name') == "Kunming Changshui International Airport"
+
+
 def test_duplicates(base):
     assert base.get('NCE', '__dup__') == set(['NCE@1'])
     assert base.get('NCE@1', '__dup__') == set(['NCE'])
