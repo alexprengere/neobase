b0176b8ead183e01749e4a9012e2dd62b9ed0d33
Alex <alexprengere@gmail.com>
2018-08-17 08:52:15 -0400

    Change updater script to exclude non IATA point of references

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env sh
 
-DIR=`dirname $0`
-cd "$DIR/neobase"
-
 rm -f optd_por_public.csv
-wget 'https://raw.githubusercontent.com/opentraveldata/opentraveldata/master/opentraveldata/optd_por_public.csv'
+wget 'https://raw.githubusercontent.com/opentraveldata/opentraveldata/master/opentraveldata/optd_por_public.csv' -O - |grep -v '^ZZZ\^' > neobase/optd_por_public.csv
