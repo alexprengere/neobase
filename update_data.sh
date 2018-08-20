beb96b40f7793ba8c0947f3aa4787711e47b4379
Alex <alexprengere@gmail.com>
2018-08-20 10:07:26 -0400

    No longer filter the raw data

--- update_data.sh
+++ update_data.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env sh
 
-wget 'https://raw.githubusercontent.com/opentraveldata/opentraveldata/master/opentraveldata/optd_por_public.csv' -O tmp.csv
-
-(head -n 1 tmp.csv && tail -n +2 tmp.csv |grep -v '^\^' |sort -t'^' -k1,1 -k42,42 -k5n,5) > neobase/optd_por_public.csv
-rm -f tmp.csv
+wget 'https://raw.githubusercontent.com/opentraveldata/opentraveldata/master/opentraveldata/optd_por_public.csv' -O neobase/optd_por_public.csv
