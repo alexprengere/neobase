81fccd97c491582b57b015fe953870e4604c33e2
Alex <alexprengere@gmail.com>
2026-02-09 11:31:46 +0100

    Use a src layout

--- update_data.sh
+++ update_data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-wget --no-check-certificate 'https://raw.githubusercontent.com/opentraveldata/opentraveldata/master/opentraveldata/optd_por_public.csv' -O neobase/optd_por_public.csv
+wget --no-check-certificate 'https://raw.githubusercontent.com/opentraveldata/opentraveldata/master/opentraveldata/optd_por_public.csv' -O src/neobase/optd_por_public.csv
